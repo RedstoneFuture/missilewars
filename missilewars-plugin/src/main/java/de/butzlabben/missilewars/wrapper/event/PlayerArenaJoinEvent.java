0f32f21facccdb3621b1f41c7b6011a969b5bb96
RedstoneFuture <redstonefuture@redstoneworld.de>
2022-08-02 14:41:34 +0200

    Make PlayerArenaJoinEvent cancelable

--- missilewars-plugin/src/main/java/de/butzlabben/missilewars/wrapper/event/PlayerArenaJoinEvent.java
+++ missilewars-plugin/src/main/java/de/butzlabben/missilewars/wrapper/event/PlayerArenaJoinEvent.java
@@ -21,6 +21,7 @@ package de.butzlabben.missilewars.wrapper.event;
 import de.butzlabben.missilewars.game.Game;
 import lombok.Getter;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
 
@@ -28,10 +29,11 @@ import org.bukkit.event.player.PlayerEvent;
  * Get's called, when a player has already entered an arena
  */
 @Getter
-public class PlayerArenaJoinEvent extends PlayerEvent {
+public class PlayerArenaJoinEvent extends PlayerEvent implements Cancellable {
 
     public final static HandlerList handlers = new HandlerList();
     private final Game game;
+    private boolean cancelled;
 
     public PlayerArenaJoinEvent(Player who, Game game) {
         super(who);
@@ -45,5 +47,15 @@ public class PlayerArenaJoinEvent extends PlayerEvent {
     @Override
     public HandlerList getHandlers() {
         return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        cancelled = b;
     }
 }
