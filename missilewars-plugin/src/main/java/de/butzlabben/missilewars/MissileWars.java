bd2efe2f27584b7057e79950a29a3ebfb3aeb871
RedstoneFuture <redstonefuture@redstoneworld.de>
2023-01-14 15:05:01 +0100

    Fix github check for PR

--- missilewars-plugin/src/main/java/de/butzlabben/missilewars/MissileWars.java
+++ missilewars-plugin/src/main/java/de/butzlabben/missilewars/MissileWars.java
@@ -20,7 +20,6 @@ package de.butzlabben.missilewars;
 
 import co.aikar.commands.PaperCommandManager;
 import de.butzlabben.missilewars.commands.MWCommands;
-import de.butzlabben.missilewars.commands.SetupCommands;
 import de.butzlabben.missilewars.commands.StatsCommands;
 import de.butzlabben.missilewars.commands.UserCommands;
 import de.butzlabben.missilewars.configuration.Config;
@@ -168,7 +167,7 @@ public class MissileWars extends JavaPlugin {
         manager.registerCommand(new MWCommands());
         manager.registerCommand(new StatsCommands());
         manager.registerCommand(new UserCommands());
-        manager.registerCommand(new SetupCommands());
+        //manager.registerCommand(new SetupCommands());
     }
 
     /**
